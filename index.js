eb29effd5be8623e93bb1cadab59ccad390df59e
Hillary Muller <hillaryaward@gmail.com>
2022-03-09 08:12:07 -0500

    commit tree 23a67fdbbb786ce96fd409c050316a25181fb5be

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(cards) {
+    const messages = [];
+        for (let i = 0; i < cards.length; i++){
+        const message = `Thank you, ${cards[i]}, for the wonderful surprise gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"]));
+
+function countDown(n){
+    for (let n = 10; n >= 0; n--){
+        console.log(n);
+    }
+}
